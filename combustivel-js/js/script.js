67481fde7138e0625dfda4f9fefc234dc5ac2224
fvcarrijo <87824768+fvcarrijo@users.noreply.github.com>
2022-07-01 21:17:51 -0300

    Add files via upload

---
+++ combustivel-js/js/script.js
@@ -0,0 +1,35 @@
+function combustivel(){
+    // Variaveis
+    var gasolina = document.getElementById('gasolina').value;
+    var etanol   = document.getElementById('etanol').value;
+    var result   = document.getElementById('result');
+
+    // Troca virgula por ponto
+    gasolina = gasolina.replace(',', '.');
+    etanol = etanol.replace(',', '.');
+
+    if(gasolina != '' && etanol != '') {
+        // Calculo melhor combustivel
+        var maisBarato = etanol/gasolina;
+
+        // algoritmo
+        if(maisBarato >= 0.7){
+            result.innerHTML = 'Melhor abastecer com gasolina.';
+        }
+        else{
+            result.innerHTML = 'Melhor abastecer com etanol.';
+        } 
+    } 
+    else{
+        alert('Preencha todos os campos.');
+    }
+}
+
+function verificarInput(event){
+    var letra = event.key;
+    if (letra != 0 && letra != 1 && letra != 2 && letra != 3 && letra != 4 && letra != 5 && letra != 6 && letra != 7 && letra != 8 && letra != 9 && letra != ',' && letra != '.' && letra != 'Tab' && letra != 'Enter' && letra != 'Backspace' && letra != 'Delete' && letra != 'ArrowLeft' && letra != 'ArrowRight'){
+        alert('caractere inválido.');
+        document.getElementById('gasolina').value = '';
+        document.getElementById('etanol').value = '';
+    }
+}
